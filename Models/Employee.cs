4057f7a84e81c6d6ce1bbcfb53fd7f6d2dba9fed
Minakshi Nandgaonkar <minakshi.nandgaonkar@niiconsulting.com>
2024-08-12 17:47:04 +0530

    Formated Form view and added validation for email.

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -18,6 +18,7 @@ namespace EmployeeInfoManagement.Models
 
         [Required]
         [EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
 
         [Required]
